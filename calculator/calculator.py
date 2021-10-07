5e006798a9edfb1a4247592cbdda17665d664e91
Sabathick <erickick13@gmail.com>
2021-10-06 21:54:47 -0300

    adicionado o botao de saida e função filtro

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,10 +1,30 @@
-class Calculator:
+import tkinter as tk
+from math import *
+import re
 
-    def sum(x,y):
-        return x+y
-    def subtract(x,y):
-        return x-y
-    def multiply(x,y):
-        return x*y
-    def division(x,y):
-        return x/y
+
+def filterMath(equation):
+    equation = re.sub('[a-zA-z,.:()" "]', '', equation)
+    print(str(equation))
+    return equation
+
+def evaluate(event):
+    equation.configure(text = "Result:" + str(eval(entry.get())))
+    
+
+app = tk.Tk()
+app.geometry('300x400')
+app.title('Calculator')
+app.resizable(False,False)
+
+tk.Label(app, text="Your expression:").pack()
+entry = tk.Entry(app)
+entry.bind("<Return>", evaluate(filterMath))
+entry.pack()
+equation = tk.Label(app)
+equation.pack()
+
+quit = tk.Button(app, text="QUIT", command=app.destroy)
+quit.pack(side="bottom")
+
+app.mainloop()
