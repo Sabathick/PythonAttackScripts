54b11d1d6fca53e53a94e32f5ad3b7bcd165cf2b
Sabathick <erickick13@gmail.com>
2021-10-31 16:18:36 -0300

    tutorialfinish

--- tutorialdjango/blog/models.py
+++ tutorialdjango/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.contrib.auth.models import User
 from django.db import models
+from django.urls import reverse
 # Create your models here.
 
 class Post(models.Model):
@@ -11,5 +12,11 @@ class Post(models.Model):
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
 
+    class Meta:
+        ordering = ("-created",)
+
     def __str__(self):
         return self.title
+    
+    def get_absolute_url(self):
+        return reverse("blog:detail", kwargs={"slug": self.slug})
