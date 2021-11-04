ca5c3fc10b5368593cf3c75896213d784ea491be
Sabathick <erickick13@gmail.com>
2021-11-03 22:09:27 -0300

    ursina organization

--- ursinaTraining/test.py
+++ ursinaTraining/test.py
@@ -1,9 +1,15 @@
 from ursina import *
 
+def update():
+    cube.x=cube.x+time.dt
+
+
+
+
+
 app = Ursina()
-
-cube = Entity(model="cube", rotation=(45,45,0))
-
+cube = Entity(model="cube", rotation=(45,45,0), color=color.rgb(255,0,0))
+txt = Text(text="This is a red cube", scale=2)
 
 app.run()
 
