ddac97876c41a676dfcd0165cba2610e4848d521
Sabathick <erickick13@gmail.com>
2021-11-06 19:49:37 -0300

    3 types of movement

--- ursinaTraining/movement.py
+++ ursinaTraining/movement.py
@@ -2,15 +2,28 @@ from ursina import *
 from random import randint
 
 def update():
-    #held key to move
+    global speed
+    cube.rotation_y = cube.rotation_y + time.dt * speed
+
+def input(key):
+    global speed
     if held_keys['r']:
-        cube.rotation_y = cube.rotation_y + time.dt * 100
+        speed=100
+    if held_keys['s']:
+        speed=0
 
 
+#def input(key):
+#move one time for each time the key is pressed
+#   if key == "r":
+#       cube.rotation_y=cube.rotation_y+time.dt * 100
 
-
-
+#def update():
+#held key to move
+#   if held_keys['r']:
+#       cube.rotation_y = cube.rotation_y + time.dt * 100
 
 app=Ursina()
+speed= 0 
 cube=Entity(model="cube", color = color.red, texture= "white_cube", scale=2)
 app.run()
