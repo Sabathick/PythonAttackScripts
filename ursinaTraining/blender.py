23394474529e2c7a822f19fff0d9f8d87d453d10
Sabathick <erickick13@gmail.com>
2021-11-11 22:58:03 -0300

    directory manage

--- ursinaTraining/blender.py
+++ ursinaTraining/blender.py
@@ -8,6 +8,6 @@ app = Ursina()
 Entity(model="quad", scale=(20,10), texture="assets/blueSky.png")
 Entity(model="quad", scale=(20,10), texture="assets/grassLongPlatform.png", z=-0.01)
 
-e=Entity(model="assets/trees/trees9.obj", scale=2, y=-1,z=-1, texture="assets/trees/Texture/Bark___0.jpg")
+e=Entity(model="assets/trees/trees9.obj", scale=2, y=-1,z=-1, texture="assets/trees/Bark___0.jpg")
 
 app.run()
