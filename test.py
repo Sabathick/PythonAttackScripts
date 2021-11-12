89c461730ec00ed0471f919a950ed13597ef32be
Sabathick <erickick13@gmail.com>
2021-11-12 16:01:33 -0300

    Sudoku solver

--- test.py
+++ test.py
@@ -1,2 +1,9 @@
-x = [3,1,-2]
-print(x[x[-1]])
+import numpy as np
+
+newBoard = np.zeros((8,8))
+
+for i in range(8):
+    for j in range(8):
+        newBoard[i,j] = input(("Digit the number of column " + str(j + 1) + " row " + str(i + 1) + ": "))
+       
+print(newBoard)
