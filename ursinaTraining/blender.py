b0db3495ea0556eac1eb9aaea94a1d3d7fd0a281
Sabathick <erickick13@gmail.com>
2021-11-11 22:54:49 -0300

    obj manipulation

---
+++ ursinaTraining/blender.py
@@ -0,0 +1,13 @@
+from ursina import *
+
+def update():
+    e.rotation_y= e.rotation_y + time.dt * 100
+
+app = Ursina()
+
+Entity(model="quad", scale=(20,10), texture="assets/blueSky.png")
+Entity(model="quad", scale=(20,10), texture="assets/grassLongPlatform.png", z=-0.01)
+
+e=Entity(model="assets/trees/trees9.obj", scale=2, y=-1,z=-1, texture="assets/trees/Texture/Bark___0.jpg")
+
+app.run()
