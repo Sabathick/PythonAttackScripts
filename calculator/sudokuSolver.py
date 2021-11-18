df0ac3525c836527c9c219d74987164ec099bbfb
Sabathick <erickick13@gmail.com>
2021-11-18 15:02:37 -0300

    deleting old board

--- calculator/sudokuSolver.py
+++ calculator/sudokuSolver.py
@@ -6,20 +6,6 @@ for i in range(9):
     for j in range(9):
         newBoard[i,j] = input(("Digit the number of column " + str(j + 1) + " row " + str(i + 1) + ": "))
        
-
-board = [
-    [7,8,0,4,0,0,1,2,0],
-    [6,0,0,0,7,5,0,0,9],
-    [0,0,0,6,0,1,0,7,8],
-    [0,0,7,0,4,0,2,6,0],
-    [0,0,1,0,5,0,9,3,0],
-    [9,0,4,0,6,0,0,0,5],
-    [0,7,0,3,0,0,0,1,2],
-    [1,2,0,0,0,7,4,0,0],
-    [0,4,9,2,0,6,0,0,7]
-]
-
-
 def solve(bo):
     find = find_empty(bo)
     if not find:
