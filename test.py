ec99bf52793618b922c634ed7de59633d9080556
Erick Pereira Martins <erickick13@gmail.com>
2022-03-07 19:03:54 -0300

    Add files via upload
    Managing the repositories

---
+++ test.py
@@ -0,0 +1,16 @@
+class Ex(Exception):
+    def __init__(self,msg):
+        Exception.__init__(self,msg +msg)
+        self.args = (msg,)
+
+try:
+    raise Ex('ex')
+except Ex as e:
+    print(e)
+except Exception as e:
+    print(e)
+
+
+
+var = []
+var.append(55)
